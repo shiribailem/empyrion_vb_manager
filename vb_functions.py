1b3776c66fd570037afa595bce651ce6660855a7
Shiri Bailem <shiri@bailem.me>
2024-11-29 18:29:39 -0600

    Initial Commit

---
+++ vb_functions.py
@@ -0,0 +1,207 @@
+import json
+from json.decoder import JSONDecodeError
+import os
+import fnmatch
+import traceback
+
+class Index_Handler:
+    def __init__(self, filename):
+        with open(filename, 'r') as file:
+            raw_keys = json.loads(file.read())
+
+        self.ids = {}
+        self.keys = {}
+
+        for key in raw_keys:
+            self.ids[raw_keys[key]] = key
+            self.keys[key] = {"id": raw_keys[key], "key": key}
+
+    def import_localization(self, filename):
+        with open(filename, 'r') as file:
+            data = file.readlines()
+
+        for line in data:
+            line = line.split(',')
+            if line[0] in self.keys.keys():
+                self.keys[line[0]]['name'] = line[1].strip()
+
+    def map_old(self, handler):
+        for key in handler.keys.keys():
+            if key in self.keys.keys():
+                self.keys[key]['alt_id'] = handler.keys[key]["id"]
+
+    def get_by_id(self, idcode):
+        return self.keys[self.ids[idcode]]
+
+
+class Item:
+    def __init__(self, data, index):
+        if isinstance(data, dict):
+            self.data = data
+            details = index.get_by_id(data['id'])
+        elif isinstance(data, str):
+            details = index.keys[data]
+            self.data = {
+                "id": details['id'],
+                "name": None,
+                "count": 1,
+                "slotIdx": 0,
+                "ammo": 0,
+                "decay": 0
+            }
+        elif isinstance(data, int):
+            details = index.get_by_id(data)
+            self.data = {
+                "id": data,
+                "name": None,
+                "count": 1,
+                "slotIdx": 0,
+                "ammo": 0,
+                "decay": 0
+            }
+        else:
+            raise
+
+        self.key = details['key']
+
+        if 'name' in details.keys():
+            self.name = details['name']
+        else:
+            self.name = self.key
+
+    def __repr__(self):
+        return f"<Item({self.name}/{self.data['count']})>"
+
+
+class Backpack:
+    def __init__(self, data, index):
+        self.slots = []
+        for x in range(49):
+            self.slots.append(None)
+        if 'Items' in data.keys() and data['Items']:
+            for raw_item in data['Items']:
+                item = Item(raw_item, index)
+                self.slots[item.data['slotIdx']] = item
+        else:
+            data['Items'] = []
+
+    def place(self, item, slot):
+        item.data['slotIdx'] = slot
+        self.slots[slot] = item
+
+    def remove(self, slot):
+        self.slots[slot] = None
+
+    def move(self, slot1, slot2):
+        item = self.slots[slot1]
+        self.place(item, slot2)
+
+    def get_raw(self):
+        data = []
+        for item in self.slots:
+            if item:
+                data.append(item.data)
+
+        return {"Items": data}
+
+    def __repr__(self):
+        output = []
+        for item in self.slots:
+            output.append(f"{item.__repr__()}")
+
+        return f"<Backpack {','.join(output)}>"
+
+
+class Master_Backpack:
+    def __init__(self, filename, index, max_vbs=10):
+        self.data = None
+        self.player = None
+        self.faction = None
+        self.locked = False
+        self.filename = filename
+        self.index = index
+        self.backpacks = []
+        self.max_vbs = max_vbs
+        self.refresh()
+
+    def refresh(self):
+        with open(self.filename, 'r') as file:
+            self.data = json.loads(file.read())
+
+        if "Backpacks" not in self.data:
+            print(f"Error loading: {self.filename}")
+            raise
+
+        self.player = self.data['LastAccessPlayerName']
+        self.faction = self.data['LastAccessFactionName']
+
+        if self.data["OpendByName"] or self.data["OpendBySteamId"]:
+            self.locked = True
+        else:
+            self.locked = False
+
+        for backpack in self.data['Backpacks']:
+            self.backpacks.append(Backpack(backpack, self.index))
+
+        while len(self.backpacks) < self.max_vbs:
+            self.backpacks.append(Backpack({}, self.index))
+
+    def get_raw(self):
+        self.data['Backpacks'] = []
+        for backpack in self.backpacks:
+            self.data['Backpacks'].append(backpack.get_raw())
+
+    def write(self):
+        with open(self.filename, 'w') as file:
+            file.write(json.dumps(self.get_raw()))
+
+    def acquire_lock(self):
+        if not self.locked or self.data["OpendByName"] == "ADMIN":
+            self.lock()
+            return True
+        else:
+            return False
+
+    def lock(self):
+        self.data["OpendByName"] = "ADMIN"
+        self.data["OpendBySteamId"] = 1
+        self.locked = True
+        self.write()
+
+    def unlock(self):
+        self.data["OpendByName"] = None
+        self.data["OpendBySteamId"] = None
+        self.locked = False
+        self.write()
+
+
+def find_json_files(directory, index, max_vbs):
+    players = {}
+    factions = {}
+    origins = {}
+    global_bp = None
+    for root, dirs, files in os.walk(directory):
+        for filename in fnmatch.filter(files, '*.json'):
+            filename = os.path.join(root, filename)
+
+            try:
+                vb = Master_Backpack(filename, index, max_vbs)
+
+                if "global.json" in filename.lower():
+                    global_bp = vb
+                elif "faction" in filename.lower():
+                    if vb.faction:
+                        factions[vb.faction] = vb
+                    else:
+                        factions[f"PRIVATE-{vb.player}"] = vb
+                elif "origin" in filename.lower():
+                    players["origin"] = vb
+                else:
+                    if vb.player:
+                        players[vb.player] = vb
+            except JSONDecodeError:
+                pass
+            except Exception:
+                traceback.print_exc()
+
+    return players, origins, factions, global_bp
---
+++ vb_manager.py
@@ -0,0 +1,214 @@
+import os
+import json
+
+from vb_functions import *
+
+
+def menu(message, raw_options, lower=True):
+    # clean options for easier matching
+    options = []
+    for option in raw_options:
+        if option:
+            option = str(option)
+            if lower:
+                option = option.lower()
+            options.append(option)
+
+    buffer = ''
+    while buffer not in options:
+        buffer = input(message)
+
+        buffer = buffer.strip()
+
+        if lower:
+            buffer = buffer.lower()
+
+        if buffer in ['q', 'quit', 'exit']:
+            return None
+
+    if lower:
+        for option in raw_options:
+            if str(option).lower() == buffer:
+                return option
+
+    return buffer
+
+
+item_index = Index_Handler("Data/NameIdMapping.json")
+
+try:
+    item_index.import_localization("Data/Localization.csv")
+except FileNotFoundError:
+    pass
+
+try:
+    with open("Data/Configuration.json", 'r') as file:
+        data = json.loads(file.read())
+        max_vbs = data['PersonalBackpack']["MaxBackpacks"]
+except FileNotFoundError:
+    max_vbs = 10
+
+try:
+    with open("Data/vbm_config.json", 'r') as file:
+        config = json.loads(file.read())
+except FileNotFoundError:
+    config = {}
+
+try:
+    directory = config['vb_path']
+except KeyError:
+    directory = None
+
+used_existing = False
+if directory:
+    print(f"Saved Virtual Backpack Directory: {directory}")
+
+    if menu("Would you like to use existing path? (Y/N)", "yn") == 'n':
+        directory = None
+    else:
+        used_existing = True
+
+if not directory:
+    directory = input("Please enter VB directory:")
+
+if not used_existing and menu("Would you like to save this path? (Y/N)", "yn") == 'y':
+    with open("Data/vbm_config.json", 'w') as file:
+        config['vb_path'] = directory
+        file.write(json.dumps(config))
+
+players, origins, factions, global_bp = find_json_files(directory, item_index, max_vbs)
+
+while True:
+    option = menu(f"Select an action:\n"
+                  f"\t1. Open an individual VB\n"
+                  f"\t2. Open a faction VB\n"
+                  f"\t3. Open origin VB\n"
+                  f"\t4. Open global VB\n"
+                  f"\t5. Search for an item by key\n"
+                  f"\t6. List all players\n"
+                  f"\t7. List all factions\n"
+                  f"\t8. List all origins\n"
+                  f"\tq. Quit Program\n\n"
+                  f"> ", list(range(1, 9)))
+
+    if not option:
+        break
+    elif option in (1, 2, 3, 4):
+        backpacks = None
+        locked = False
+        if option == 1:
+            handle = menu("Enter the player id you wish to open (case insensitive): ", players.keys())
+            if handle:
+                handle = players[handle]
+                backpacks = handle.backpacks
+        elif option == 2:
+            handle = menu("Enter the faction id you wish to open (case insensitive): ", factions.keys())
+            if handle:
+                handle = factions[handle]
+                backpacks = handle.backpacks
+        elif option == 3:
+            handle = menu("Enter the origin id you wish to open (case insensitive): ", origins.keys())
+            if handle:
+                handle = origins[handle]
+                backpacks = handle.backpacks
+        else:
+            handle = global_bp
+            backpacks = global_bp.backpacks
+
+        if handle.locked:
+            print("\nWARNING BACKPACK IS LOCKED! MAKE SURE PLAYER ISN'T USING IT!\n")
+
+        if backpacks:
+            print(f"{handle.player}'s backpacks:")
+
+            count = 0
+            for backpack in backpacks:
+                print(f"\t{count}: {len(list(filter(lambda x: x, backpack.slots)))} items")
+                count += 1
+
+            print("\tr: Refresh Backpacks")
+
+            option = menu("> ", [*range(max_vbs), 'r'])
+
+            if option == 'r':
+                handle.refresh()
+                print(f"Refreshed Backpack. Backpack is Locked:{handle.locked}.")
+                option = None
+
+            while option is not None:
+                backpack = backpacks[int(option)]
+
+                while option:
+                    count = 0
+                    for item in backpack.slots:
+                        if item:
+                            print(f"{count}: {item.data['count']} x {item.name}")
+                        else:
+                            print(f"{count}: Empty")
+                        count += 1
+
+                    option = menu("\nSelect an action:\n"
+                                  "\t1: Add/replace Item\n"
+                                  "\t2: Remove Item\n"
+                                  "\t3: Change Quantity\n"
+                                  "\t4: Reset Decay\n"
+                                  "\tr: Refresh Backpack\n"
+                                  "\tq: Go back to previous menu\n\n"
+                                  "> ", [1, 2, 3, 4, 'r'])
+
+                    if option == 'r':
+                        handle.refresh()
+                        print(f"Refreshed Backpack. Backpack is Locked:{handle.locked}.")
+                    elif option:
+                        slot = menu("Enter the slot number to update: ", list(range(0, 49)))
+
+                        if slot is not None:
+                            slot = int(slot)
+                            if option == 1:
+                                item_key = menu("Enter the item key you wish to add/replace: ",
+                                                item_index.keys.keys())
+                                if item_key:
+                                    quantity = menu("Enter the quantity: ", range(1, 50000))
+                                    if quantity:
+                                        item = Item(item_key, item_index)
+                                        item.data['count'] = quantity
+                                        backpack.place(item, slot)
+                                        print(f"Added {quantity} of {item_key}")
+
+                            elif option == 2:
+                                item = backpack.slots[slot]
+                                if item:
+                                    print(f"Removed {item.name}")
+                                    backpack.remove(slot)
+                                else:
+                                    print("Slot already empty")
+
+                            elif option == 3:
+                                quantity = menu("Enter the new quantity: ", range(1, 50000))
+                                if quantity:
+                                    backpack.slots[slot].data['count'] = int(quantity)
+
+                            elif option == 4:
+                                item = backpack.slots[slot]
+                                if item:
+                                    item.data['decay'] = 0
+                                    print(f"Reset decay on {item.name}")
+                                else:
+                                    print("No item in slot.")
+    elif option in [6, 7, 8]:
+        handle = [players, factions, origins][option-6]
+
+        if option == 6:
+            print("All players:")
+        elif option == 7:
+            print("All Factions:")
+        elif option == 8:
+            print("All Origins:")
+            print("--- ORIGIN SEARCH NOT IMPLEMENTED ---")
+
+        for item in sorted(handle.keys()):
+            if option == 6:
+                print(item)
+            elif option == 7:
+                if item.faction:
+                    print(item.faction)
